eb18f5700a834c6d21931e588f490b28e805352d
07jonathan <93192031+07jonathan@users.noreply.github.com>
2022-01-05 12:01:40 +0700

    Delete pendaftaran directory

--- pendaftaran/src/Controller/ControllerInterfaces.java
+++
@@ -1,6 +0,0 @@
-package Controller;
-
-public interface ControllerInterfaces {
-    int login(String npm, String password);
-    
-}
--- pendaftaran/src/Controller/PaketConInterface.java
+++
@@ -1,9 +0,0 @@
-package Controller;
-
-import Entity.PaketEntity;
-import java.util.ArrayList;
-
-public interface PaketConInterface {
-    ArrayList<PaketEntity> showAllDataA();
-    ArrayList<PaketEntity> showAllDataB();
-}
