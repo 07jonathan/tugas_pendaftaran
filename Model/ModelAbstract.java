efe92e5432f49997b1c59cbf3814d8b3aed9891e
07jonathan <93192031+07jonathan@users.noreply.github.com>
2022-01-05 13:54:49 +0700

    Delete Model directory

--- Model/ModelAbstract.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-import Helper.KoneksiDb;
-import java.sql.Connection;
-
-public abstract class ModelAbstract {
-    Connection conn = KoneksiDb.getconnection();
-    public abstract int cekLogin(String npm, String Password);
-}
